4110a1d1b1ae7e2e8d691afe94cfd7829b85a259
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-05-19 23:27:12 -0300

    feat: padronizando os erros com o padrão either

---
+++ src/config/errors/either.ts
@@ -0,0 +1,41 @@
+class Right<L, R> {
+  readonly value: R;
+
+  constructor(value: R) {
+    this.value = value;
+  }
+
+  isRight(): this is Right<L, R> {
+    return true;
+  }
+
+  isLeft(): this is Right<L, R> {
+    return false;
+  }
+}
+
+class Left<L, R> {
+  readonly value: L;
+
+  constructor(value: L) {
+    this.value = value;
+  }
+
+  isRight(): this is Left<L, R> {
+    return false;
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true;
+  }
+}
+
+export type Either<L, R> = Right<L, R> | Left<L, R>;
+
+export const right = <L, R>(value: R): Either<L, R> => {
+  return new Right(value);
+};
+
+export const left = <L, R>(value: L): Either<L, R> => {
+  return new Left(value);
+};
